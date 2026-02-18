89aabb8024caa3cba40cc5373805de1c8c27d7af
yonal5 <yonalabasingha@gmail.com>
2026-02-18 09:38:46 +0530

    Update productPage.jsx

--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -140,9 +140,7 @@ export function ProductPage() {
 
   return (
 
-    <div className="w-full min-h-[calc(100vh-100px)] bg-orange-100">
-
-      <Header />
+    <div className="w-full min-h-[calc(100vh-100px)] bg-orange-100">  
 
       {
 
